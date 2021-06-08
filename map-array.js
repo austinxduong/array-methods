fe89d0f77abb23d3baf588e8ee9a9e0e1129ad46
austinxduong <axduong2@gmail.com>
2021-06-08 16:34:05 -0700

    setup test for map-array

---
+++ map-array.js
@@ -0,0 +1,3 @@
+export default map(arr, callback) {
+    for ()
+}
---
+++ map-array.test.js
@@ -0,0 +1,10 @@
+import map from './map-array.js';
+
+describe('map', () => {
+  it('takes an array and callback of signature and creates new array with return value of each called callback', () => {
+    const numbers = [1, 2, 3];
+    const newArray = 
+    expect(newArray).
+    // expect(numbers). toEqual([1, 2, 3]);
+  });
+}); 
