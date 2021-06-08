eb466d773bb6b58bfa3ea5e0f1ddd8b6952a6ce7
austinxduong <axduong2@gmail.com>
2021-06-08 16:40:21 -0700

    more accurate test

--- map-array.test.js
+++ map-array.test.js
@@ -2,9 +2,13 @@ import map from './map-array.js';
 
 describe('map', () => {
   it('takes an array and callback of signature and creates new array with return value of each called callback', () => {
-    const numbers = [1, 2, 3];
-    const newArray = 
-    expect(newArray).
+    const array = [1, 2, 3];
+    const actual = callback(array, (num) => num + 1);
+    expect (actual).toEqual([2, 3, 4]);
+
+
+    // const newArray = 
+    // expect(newArray).
     // expect(numbers). toEqual([1, 2, 3]);
   });
 }); 
